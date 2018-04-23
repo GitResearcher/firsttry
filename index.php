9512e6981d50086ef14cf49fe64525995a34f6e7
GitResearcher <ushanov_a@mail.ru>
2018-04-23 17:25:23 +0300

    HUETA

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: GitResearcher
+ * Date: 23.04.2018
+ * Time: 16:51
+ */
+
+header ("Content-Type: text/html; charset=utf-8");
+echo "<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<title>Добро пожаловать!</title>
+</head>
+
+<body style='text-align: center'>
+    <table style='width:100%;height:100%;border:none'>
+        <tbody>
+            <tr>
+                <td style='padding: 20px'>
+                    <div class='authform'>
+                        <h3>Добро пожаловать!</h3>
+                        <div style='margin-bottom: 18px'>Open Server Panel работает ;-)</div>
+                        <a href='https://ospanel.io/docs/' id='link' style='color: #4D5662'>Руководство пользователя</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>";
